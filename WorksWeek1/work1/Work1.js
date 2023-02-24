aeb3dc4952e02077b033466fa5b9c4a0b2c2377c
Fatma Demir <84198092+ftmd909@users.noreply.github.com>
2023-02-24 20:21:40 +0300

    Update Work1.js

--- WorksWeek1/work1/Work1.js
+++ WorksWeek1/work1/Work1.js
@@ -1,13 +1,19 @@
 const Factorial = (value) => {
-    if(typeof value === "number"){
+    if (value < 0 || value === 0) {
+        console.log("factorial value is 0 and cannot be less than 0!:",value);
+    }
+   else if(typeof value === "number" && value > 0){
         let output = 1;
-        for(let i = 1 ; i < value ; i++){
+        for(let i = 1 ; i <= value ; i++){
             output *= i;
         }
         console.log(output);
     }
     else{
-        console.log("Please enter value in number type!");
+        console.log("Please enter value in number type!:",value);
     }
 }
 Factorial(3);
+Factorial(-8);
+Factorial(0);
+Factorial("Fatma");

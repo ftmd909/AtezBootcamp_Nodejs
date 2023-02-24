91502a54e5d33682f003620778bfc6ebcf35a959
ftmd909 <ftmd909@gmail.com>
2023-02-24 20:15:25 +0300

    commit

--- work1/Work1.js
+++ work1/Work1.js
@@ -1,7 +1,10 @@
 const Factorial = (value) => {
-    if(typeof value === "number"){
+    if (value < 0 || value === 0) {
+        console.log("factorial value is 0 and cannot be less than 0!");
+    }
+   else if(typeof value === "number" && value > 0){
         let output = 1;
-        for(let i = 1 ; i < value ; i++){
+        for(let i = 1 ; i <= value ; i++){
             output *= i;
         }
         console.log(output);
